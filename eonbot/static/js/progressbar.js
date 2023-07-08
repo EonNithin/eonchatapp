a398d3be3dd09f509c174b562640e30f4e2bcf09
nithin <nithin@learneon.com>
2023-07-08 21:59:31 +0530

    commit changes

--- eonbot/static/js/progressbar.js
+++ eonbot/static/js/progressbar.js
@@ -1,10 +1,6 @@
-
 document.getElementById("myForm").addEventListener("submit", function(event) {
     // Prevent form submission
     //event.preventDefault();
-
-    // Hide the form
-    document.getElementById("myForm").style.display = "none";
 
     // Show the progress bars
     var progressBars = document.querySelectorAll(".progress-bar");
@@ -12,32 +8,21 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
         progressBar.style.display = "block";
     });
 
-    // Animate the progress bars
-    var widths = [100, 200, 300]; // Widths of the progress bars
-    var progressBarFills = document.querySelectorAll(".progress-fill");
+    // Disable the submit button
+    var submitButton = document.querySelector(".btn-primary");
+    submitButton.disabled = true;
 
-    var animationCount = widths.length;
+    // Simulate the loading delay (replace with your actual asynchronous request)
+    setTimeout(function() {
+        // Hide the progress bars
+        progressBars.forEach(function(progressBar) {
+           progressBar.style.display = "none";
+        });
 
-    widths.forEach(function(width, index) {
-        var progressBarFill = progressBarFills[index];
-        var currentWidth = 0;
-        var interval = setInterval(function() {
-            if (currentWidth >= width) {
-                clearInterval(interval);
-                animationCount--;
+        // Enable the submit button
+        submitButton.disabled = false;
 
-                if (animationCount === 0) {
-                    // Hide the progress bars after all animations complete
-                    progressBars.forEach(function(progressBar) {
-                        progressBar.style.display = "none";
-                    });
-                    // Show the form again
-                    document.getElementById("myForm").style.display = "block";
-                }
-            } else {
-                currentWidth++;
-                progressBarFill.style.width = currentWidth + "px";
-            }
-        }, 5); // Adjust the interval to control the animation speed
-    });
+        // Optional: Reset the form
+        document.getElementById("myForm").reset();
+    }, 5000); // Adjust the timeout value or replace with your actual request
 });
