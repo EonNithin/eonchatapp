4991c4b575d98c47adfb34ac61193d66d3809861
nithin <nithin@learneon.com>
2023-07-14 12:10:11 +0530

    commit changes

--- eonbot/views.py
+++ eonbot/views.py
@@ -10,37 +10,21 @@ import pandas as pd
 from django.templatetags.static import static
 
 # CSV file data importing and processing
-csv_file_path = os.path.join(eonchatapp.settings.STATIC_ROOT, 'CBSE Syllabus class9&10.csv')
+csv_file_path = os.path.join(eonchatapp.settings.STATIC_ROOT, 'CBSE Syllabus.csv')
 # static('csv_files/CBSE-Syllabus.csv')  # Update with the actual CSV file name
-df = pd.read_csv(csv_file_path)
-print('begin dataframe\n'+'='*50)
-print(df)
-print('end dataframe\n'+'='*50)
 
+df = pd.read_csv(csv_file_path)
+# Extract relevant information from the CSV data
 # Handle missing values
 df = df.stack().reset_index(drop=True).to_frame()
-df = df.dropna() # Drop rows with missing values
-print('begin dataframe after dropped missing values\n'+'='*50)
-print(df)
-print('end dataframe\n'+'='*50)
+relevant_info = df.dropna() # Drop rows with missing values
 
-# Convert the DataFrame to a JSON serializable format
-json_data = df.to_dict(orient='list')
-print('begin dataframe\n'+'='*50)
-print(json_data)
-print('end dataframe\n'+'='*50)
+# Convert the extracted information to a string
+syllabus_keywords = relevant_info_str = ','.join(df.values.flatten())
 
-# Convert the JSON serializable data to a string
-data_str = json.dumps(json_data)
-print('begin dataframe\n'+'='*50)
-print(data_str)
-print('end dataframe\n'+'='*50)
-
-# Set of syllabus-related keywords
-syllabus_keywords = data_str
-print('begin syllabus_keywords\n'+'='*50)
-print(syllabus_keywords)
-print('end syllabus_keywords\n'+'='*50)
+print('begin dataframe'+'='*50)
+print(relevant_info_str)
+print('end dataframe'+'='*50)
 
 # Variable to store the conversation history
 conversation_history = []
@@ -57,7 +41,6 @@ def get_chatgpt_response(question):
 
     if is_syllabus_related_question(question):
         # API implementation
-
         # create openai instance
         openai.Model.list()
         # Set the API key -- Get OpenAI api key from environment variables path(eonchatapp-m3) done in settings.py file.
@@ -72,20 +55,24 @@ def get_chatgpt_response(question):
         prompt = "\n".join(conversation_history)
 
         if prompt=='':
-            prompt = 'Answer as consisely as possible'+syllabus_keywords
+            prompt = 'please provide information about '+relevant_info_str
 
         # Make a Completion
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt,
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": question}
+            ],
             temperature=0.5,
             max_tokens=300,
             top_p=0,
             frequency_penalty=0,
             presence_penalty=0
         )
-        # Parse the response
-        response = (response['choices'][0]['text']).strip()
+        # Get the last response from the API
+        response = response['choices'][0]['message']['content'] if 'choices' in response else response['message'][
+            'content']
 
         # Append the current question and AI response to the conversation history
         conversation_history.append(response)
