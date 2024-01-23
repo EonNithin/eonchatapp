df03b0114d804a501684a0b0dc2df0162c019114
Nithin <nithin@learneon.com>
2024-01-23 13:38:38 +0530

    save changes

--- eonbot/urls.py
+++ eonbot/urls.py
@@ -3,6 +3,11 @@ from eonbot import views
 from eonbot.views import home
 from eonbot.views import response_view
 
+from django.urls import include, path, re_path
+from django.views.static import serve
+from eonchatapp import settings
+
+
 urlpatterns = [
     path('', home, name='home'),
     path('response/', views.response_view, name='response_view'),  # Add this line for the response view
