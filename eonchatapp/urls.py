074ad6b9d3b594c27296010b3dfedd057058a1ed
Nithin <nithin@learneon.com>
2024-01-24 11:55:29 +0530

    save changes

--- eonchatapp/urls.py
+++ eonchatapp/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('', include("eonbot.urls")),
     
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
