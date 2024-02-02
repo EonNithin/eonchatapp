1831205bb0c7fba320acbd9ad0dbe537366cee18
Nithin <nithin@learneon.com>
2024-02-02 10:08:27 +0530

    save changes

--- eonchatapp/settings.py
+++ eonchatapp/settings.py
@@ -115,11 +115,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'eonbot/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
